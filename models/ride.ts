570cf5cc2a9740c0cb782656bce7c4fa045de6d9
agomezmo <usedano@gmail.com>
2018-03-01 20:27:13 +0100

    sort by distance

--- models/ride.ts
+++ models/ride.ts
@@ -6,6 +6,7 @@ export class ride{
     end: coordinate;
     earlyStart: number;
     latestFinish: number;
+    distance: number = 0;
 
     constructor(id:number, a:number,b:number,x:number,y:number,s:number,f:number){
         this.id = id;
@@ -13,5 +14,21 @@ export class ride{
         this.end = new coordinate(x, y);
         this.earlyStart = s;
         this.latestFinish = f;
+    }
+
+   public sortRidesByDistance(rides: ride[]) {
+        rides.forEach(ride => {
+            ride.distance = Math.abs(ride.start.x - ride.end.y) + Math.abs(ride.end.x - ride.end.y);
+        });
+    
+        rides.sort((a,b) => {
+            if(a.distance < b.distance) {
+                return -1;
+            } else if(a.distance > b.distance) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
     }
 }
